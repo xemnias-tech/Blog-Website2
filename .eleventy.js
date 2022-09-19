8d65e7cb67c5a0eab58f1a0e98467677d2c1450a
xemnias-tech <77797838+xemnias-tech@users.noreply.github.com>
2022-09-20 00:48:11 +0800

    update

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     })
+    
     return {
         dir: {
             input : "src",
